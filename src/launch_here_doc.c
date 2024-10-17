13f30391d5ea6001bfaa9d16bb58030bbfd7c2e5
azakharo <zarart@yandex.ru>
2024-10-18 00:47:30 +0500

    Change parcer to args[][]

--- src/launch_here_doc.c
+++ src/launch_here_doc.c
@@ -6,7 +6,7 @@
 /*   By: artemii <artemii@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 21:17:06 by mmychaly          #+#    #+#             */
-/*   Updated: 2024/10/10 00:37:57 by artemii          ###   ########.fr       */
+/*   Updated: 2024/10/18 00:28:10 by artemii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	launch_here_doc(char **argv, int pipefd[2])
 
 	line = NULL;
 	close(pipefd[0]);
-	printf("in launch_here_doc\n"); //Supprimer
+	ft_printf("in launch_here_doc\n"); //Supprimer
 	while ((line = get_next_line(0)) != NULL)
 	{
 		if ((ft_strncmp(argv[2], line, ft_strlen(line) - 1)) == 0)
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: artemii <artemii@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:16:35 by azakharo          #+#    #+#             */
-/*   Updated: 2024/10/15 01:44:09 by artemii          ###   ########.fr       */
+/*   Updated: 2024/10/18 00:38:20 by artemii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,75 +14,86 @@
 
 extern char	**environ;
 
-void	print_command(t_command *command) //Просто для дебага
+void print_commands(t_command *command)
 {
-	int	j;
+    int i, j;
 
-	ft_printf("Commands:\n");
-	j = 0;
-	while (command->args[j] != NULL) 
-	{
-		ft_printf("  Args[%d]: %s \n", j, command->args[j]);
-		j++;
-	}
-	if (command->input_file)
-		ft_printf("  Input file: %s\n", command->input_file);
-	if (command->output_file)
-		ft_printf("  Output file: %s\n", command->output_file);
-	if (command->append)
-		ft_printf("  Append mode: enabled\n");
-	if (command->here_doc)
-		ft_printf("  Here_doc: %s\n", command->here_doc);
-	if (command->is_pipe)
-		ft_printf("  Is pipe: enabled\n");
+    i = 0;
+    while (command->cmd[i] != NULL)
+    {
+        ft_printf("Command[%d]:\n", i);
+        j = 0;
+        while (command->cmd[i][j] != NULL)
+        {
+            ft_printf("  Args[%d]: %s\n", j, command->cmd[i][j]);
+            j++;
+        }
+        i++;
+    }
+    if (command->input_file)
+        ft_printf("  Input file: %s\n", command->input_file);
+    if (command->output_file)
+        ft_printf("  Output file: %s\n", command->output_file);
+    if (command->append)
+        ft_printf("  Append mode: enabled\n");
+    if (command->here_doc)
+        ft_printf("  Here_doc: enabled\n");
+    if (command->is_pipe)
+        ft_printf("  Is pipe: enabled\n");
 }
 
-void	free_commands(t_command *cmd)
-{
-	int	i;
 
-	if (cmd->args)
-	{
-		i = 0;
-		while (cmd->args[i])
-		{
-			free(cmd->args[i]);
-			i++;
-		}
-		free(cmd->args);
-	}
-	if (cmd->input_file)
-		free(cmd->input_file);
-	if (cmd->output_file)
-		free(cmd->output_file);
-	free(cmd);
+void free_structure(t_command *cmd) //Функция для чистки структуры
+{
+    int i = 0;
+    
+    // Освобождаем каждый массив аргументов
+    if (cmd->cmd) 
+    {
+        while (cmd->cmd[i]) 
+        {
+            free_split(cmd->cmd[i]);  // Освобождаем каждый массив
+            i++;
+        }
+        free(cmd->cmd);  // Освобождаем сам массив команд
+    }
+    // Освобождаем файлы редиректов
+    if (cmd->input_file)
+        free(cmd->input_file);
+    if (cmd->output_file)
+        free(cmd->output_file);
+    // Освобождаем саму структуру
+    free(cmd);
 }
 
-int	main(void)
+int main(void)
 {
-	char		*user_input;
-	char		**envp;
-	t_command	*commands;
+    char *user_input;
+    t_command *command;
 
-	envp = environ;
-	while (1)
-	{
-		user_input = readline("minishell$ ");
-		if (!user_input)
-			break ;
-		if (ft_strlen(user_input) > 0)
-			add_history(user_input);
-		// Парсим введённую строку и получаем структуру команды
-		commands = parse_pipeline(user_input, envp);
-		if (commands)
-		{
-			print_command(commands); // для дебага
-			execute_pipeline(commands);
-			free_commands(commands);
-			// Функция для освобождения структуры команды
-			free(user_input);
-		}
-	}
-	rl_clear_history();
-	return (0);
+    while (1)
+    {
+        user_input = readline("minishell$ ");
+        if (!user_input)
+            break;
+        if (ft_strlen(user_input) > 0)
+            add_history(user_input);
+        command = malloc(sizeof(t_command));
+        if (!command)
+        {
+            perror("malloc failed");
+            return (1);
+        }
+        ft_memset(command, 0, sizeof(t_command));
+        parse_pipeline(command, user_input);
+
+        print_commands(command); // Вывод в консоль для дебага
+        //ВОТ СЮДА НАДО ДОБАВИТЬ EXECUTION
+        free_structure(command);
+        free(user_input);
+    }
+
+    rl_clear_history();
+    return 0;
 }
+
