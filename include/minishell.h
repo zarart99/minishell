228227144cf9a1e224036c81ad25ecebdf0159c7
azakharo <zarart@yandex.ru>
2024-10-24 01:24:28 +0500

    First version

--- include/minishell.h
+++ include/minishell.h
@@ -17,7 +17,7 @@ typedef struct s_cmd
     char *here_doc_file;  // Для "<<"
     int pos_here_doc;     // Позиция для приоритета here_doc
     char *cmd;            // Основная команда (например, "echo")
-    char *cmd_arg;        // Аргументы команды (например, "hello world")
+    char **cmd_arg;        // Аргументы команды (например, "hello world")
     char *output_file;    // Для ">"
     char *append_file;    // Для ">>"
     int pos_output;       // Позиция для приоритета вывода
--- src/exec/execution_utils_2.c
+++ src/exec/execution_utils_2.c
@@ -3,25 +3,61 @@
 /*                                                        :::      ::::::::   */
 /*   execution_utils_2.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmychaly <mmychaly@student.42.fr>          +#+  +:+       +#+        */
+/*   By: artemii <artemii@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 04:19:01 by mmychaly          #+#    #+#             */
-/*   Updated: 2024/10/22 04:39:19 by mmychaly         ###   ########.fr       */
+/*   Updated: 2024/10/24 01:22:24 by artemii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "../../include/minishell.h"
 
-char **join_arg(t_data *data)
+char	*join_cmd_args(char **cmd_arg)
 {
-	char **temp;
+	int		i;
+	char	*joined_args;
+	char	*temp;
+	size_t	total_length;
+
+	total_length = 0;
+	i = 1;
+	if (!cmd_arg || !cmd_arg[0])
+		return (NULL);
+	while (cmd_arg[i])
+	{
+		total_length += ft_strlen(cmd_arg[i]) + 1;
+		i++;
+	}
+	joined_args = malloc(total_length + 1);
+	if (!joined_args)
+		return (NULL);
+	joined_args[0] = '\0';
+	i = 1;
+	while (cmd_arg[i])
+	{
+		if (ft_strlen(joined_args) > 0)
+		{
+			temp = joined_args;
+			joined_args = ft_strjoin(joined_args, " ");
+			free(temp);
+		}
+		temp = joined_args;
+		joined_args = ft_strjoin(joined_args, cmd_arg[i]);
+		free(temp);
+		i++;
+	}
+	return (joined_args);
+}
+
+char	**join_arg(t_data *data)
+{
+	char	**temp;
 
 	temp = malloc(sizeof(char *) * 3);
 	if (temp == NULL)
 		return (NULL);
 	temp[0] = data->cmd[data->i]->cmd;
-	temp[1] = data->cmd[data->i]->cmd_arg;
+	temp[1] = join_cmd_args(data->cmd[data->i]->cmd_arg);
 	temp[2] = NULL;
 	return (temp);
 }
