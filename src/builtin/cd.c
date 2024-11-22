f7066cc0085cc049837d0a60ecbedcb4db1bf8c4
mmychaly <mmychalyszyn@gmail.com>
2024-11-22 07:53:46 +0100

    mod return in cd

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -44,10 +44,11 @@ static int change_directory(t_data *data, char *target_dir)
 // Обновляет переменные окружения OLDPWD и PWD
 static void update_environment(t_data *data, char *old_pwd)
 {
-    char *new_pwd = get_current_directory(data);
+    char *new_pwd;
     char *export_oldpwd = NULL;
     char *export_newpwd = NULL;
 
+    new_pwd = get_current_directory(data);
     if (!new_pwd)
         return;
     export_oldpwd = ft_strjoin("OLDPWD=", old_pwd);
@@ -67,10 +68,13 @@ static void update_environment(t_data *data, char *old_pwd)
 
 void cd(t_data *data, char *arg)
 {
-    char *old_pwd = get_current_directory(data);
+    char *old_pwd;
     char *target_dir = NULL;
     int free_target_dir = 0; // Флаг, указывающий, нужно ли освобождать target_dir
 
+    if (data->nb_pipe != 0)//Закрываем input для составных команд
+		close_input(data);
+    old_pwd = get_current_directory(data);
     if (!old_pwd)
         return;
     if (!arg) // Если аргумент отсутствует, используем "HOME"
@@ -84,20 +88,31 @@ void cd(t_data *data, char *arg)
         target_dir = get_env_value("OLDPWD", data);
         if (!target_dir)
             cd_exit(data, 1, "cd: OLDPWD not set\n", old_pwd);
-        ft_printf("%s\n", target_dir); // Печатаем OLDPWD
+        else
+            ft_printf("%s\n", target_dir); // Печатаем OLDPWD
     }
     else if (arg[0] == '~')
     {
         char *home = get_env_value("HOME", data);
         if (!home)
             cd_exit(data, 1, "cd: HOME not set\n", old_pwd);
-        target_dir = ft_strjoin(home, arg + 1);
-        free_target_dir = 1; // Устанавливаем флаг, так как строка была создана
+        else
+        {
+            target_dir = ft_strjoin(home, arg + 1);
+            free_target_dir = 1; // Устанавливаем флаг, так как строка была создана
+        }
     }
     else
         target_dir = arg;
+    if (data->back_in_main == 1)
+        return ;
+    //Если выше в условиях неполучилось создать строку с адресом то cd_exit проставит 
+    //data->back_in_main == 1 , значит когда выпадем с уловия то в случае ошибки здесь 
+    //будет выход из функции.Я не прописывал выход в каждом условии что бы не забивать место 
     if (change_directory(data, target_dir) == 0)
         update_environment(data, old_pwd);
+    if (data->back_in_main == 1)//Если change_directory или update_environment отработали не корректно то выходим из функции обратно в main
+        return ;
     if (free_target_dir)    // Освобождаем target_dir только если оно было выделено динамически
         free(target_dir);
     cd_exit(data, 0, NULL, old_pwd);
